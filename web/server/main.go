e2a72fa408b07797e89e83cdf2f9859c15710061
aaochieng <ochiengaoron@outlook.com>
2024-11-20 11:10:08 +0300

    fix: static files server

--- web/server/main.go
+++ web/server/main.go
@@ -5,13 +5,27 @@ import (
 	"forum/web/server/routes"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func main() {
-	http.HandleFunc("/", routes.HomeRoute)
-	fmt.Println("Server running on http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
+	// Define the path to the static files
+	staticDir := "./web/static/" // Relative path from `server/main.go` to `web/static`
+
+	// Get the absolute path for better error handling
+	absStaticDir, err := filepath.Abs(staticDir)
 	if err != nil {
+		log.Fatalf("Failed to get absolute path of static directory: %v", err)
+	}
+
+	// Serve static files
+	fs := http.FileServer(http.Dir(absStaticDir))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	log.Printf("Serving static files from %s on http://localhost:8080/static/", absStaticDir)
+	http.HandleFunc("/", routes.RoutesHandler)
+	fmt.Println("Server running on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe error:", err)
 	}
 }
