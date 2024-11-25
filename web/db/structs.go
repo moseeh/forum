9ee5ceb049918a1aa2fb0e4cdb95596c2fe15fbd
aaochieng <ochiengaoron@outlook.com>
2024-11-25 14:02:54 +0300

    add: uuid generator method

--- web/db/structs.go
+++ web/db/structs.go
@@ -1,7 +1,16 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/gofrs/uuid"
+)
 
 type ForumDB struct {
 	db *sql.DB
+}
+
+func (conn ForumDB) genUUID() string {
+	u2, _ := uuid.NewV4()
+	return u2.String()
 }
