e160d4de42e66cda91c751eb87dbf96de555167d
moonyango <mosesadrian825@gmail.com>
2024-12-05 12:14:56 +0300

    fix: check if theres a valid session cookie before executing a login page

--- cmd/web/login.go
+++ cmd/web/login.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
-	"forum/internal"
 	"html/template"
 	"net/http"
 	"time"
+
+	"forum/internal"
 )
 
 func (app *App) GetLoginHandler(w http.ResponseWriter, r *http.Request) {
+	sessionCookie, err := r.Cookie("session_token")
+	if err == nil {
+		valid, validationErr := app.users.ValidateSession(sessionCookie.Value)
+		if validationErr == nil && valid {
+			http.Redirect(w, r, "/home", http.StatusSeeOther)
+			return
+		}
+		http.SetCookie(w, &http.Cookie{
+			Name:     "session_token",
+			Value:    "",
+			Path:     "/",
+			Expires:  time.Now().Add(-1 * time.Hour),
+			HttpOnly: true,
+		})
+	}
 	tmpl, err := template.ParseFiles("./assets/templates/login.page.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -18,7 +34,7 @@ func (app *App) GetLoginHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *App) PostLoginHandler(w http.ResponseWriter, r *http.Request) {
-	var tmpl = template.Must(template.ParseFiles("./assets/templates/login.page.html"))
+	tmpl := template.Must(template.ParseFiles("./assets/templates/login.page.html"))
 	form_errors := map[string][]string{}
 	if err := r.ParseForm(); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -37,13 +53,13 @@ func (app *App) PostLoginHandler(w http.ResponseWriter, r *http.Request) {
 		form_errors["password"] = append(form_errors["password"], "Password must be at least 8 characters")
 	}
 
-	//confirm if a user exists in the database
+	// confirm if a user exists in the database
 	if ok, _ := app.users.UserExists(email); !ok {
 		http.Error(w, "User already exists", http.StatusFound)
 		return
 	}
 
-	//Get password hash
+	// Get password hash
 	password_hash, err := app.users.GetPassword(email)
 	if err != nil {
 		fmt.Println(err)
@@ -62,7 +78,7 @@ func (app *App) PostLoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//get username
+	// get username
 	user_id, username, err := app.users.GetUsername(email)
 	if err != nil {
 		fmt.Println(err)
@@ -80,7 +96,7 @@ func (app *App) PostLoginHandler(w http.ResponseWriter, r *http.Request) {
 		HttpOnly: true,
 	})
 
-	//set csfr token
+	// set csfr token
 	http.SetCookie(w, &http.Cookie{
 		Name:     "csrf_token",
 		Value:    csrf_token,
@@ -88,7 +104,7 @@ func (app *App) PostLoginHandler(w http.ResponseWriter, r *http.Request) {
 		HttpOnly: false,
 	})
 
-	//set username
+	// set username
 	http.SetCookie(w, &http.Cookie{
 		Name:     "username",
 		Value:    username,
