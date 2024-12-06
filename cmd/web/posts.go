f526be1df2973a773f465146ab0c0528702425f2
moonyango <mosesadrian825@gmail.com>
2024-12-06 15:32:41 +0300

    fix: user post values to call other functions

--- cmd/web/posts.go
+++ cmd/web/posts.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"forum/internal"
 )
 
 func (app *App) PostsHandler(w http.ResponseWriter, r *http.Request) {
-	sessionCookie, err := r.Cookie("session_token")
-	if err == nil {
-		usernameCookie, err := r.Cookie("username")
-		if err == nil {
-			fmt.Println(usernameCookie.Value)
-			fmt.Println(sessionCookie.Value)
-			title := r.FormValue("title")
-			content := r.FormValue("content")
-			fmt.Println(title, content)
-		}
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	_, err := r.Cookie("session_token")
+	if err != nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
 
+	}
+	usernameCookie, err := r.Cookie("username")
+	if err != nil {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	}
+
+	title := r.FormValue("title")
+	content := r.FormValue("content")
+
+	if title == "" || content == "" {
+		http.Error(w, "title and content are required", http.StatusBadRequest)
+		return
+	}
+
+	user_id, err := app.users.GetUserID(usernameCookie.Value)
+	if err != nil {
+		http.Error(w, "Error Getting user ID", http.StatusInternalServerError)
+		return
+	}
+	post_id := internal.UUIDGen()
+	err = app.users.InsertPost(post_id, title, content, user_id)
+	if err != nil {
+		http.Error(w, "Error making post", http.StatusInternalServerError)
+		return
 	}
 }
