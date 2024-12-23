b04ea9b98b1b5d59047d3168c0f351ecbbdfe7a5
moonyango <mosesadrian825@gmail.com>
2024-12-23 18:26:37 +0300

    feat: comments handler to add comment to the database

---
+++ cmd/web/comments.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"forum/internal"
+)
+
+func (app *App) CommentsHandler(w http.ResponseWriter, r *http.Request) {
+	usernameCookie, err := r.Cookie("username")
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	user_id, err := app.users.GetUserID(usernameCookie.Value)
+	if err != nil {
+		http.Error(w, "Error getting user ID from the databse", http.StatusInternalServerError)
+		return
+	}
+	post_id := r.FormValue("post_id")
+	comment := r.FormValue("comment")
+
+	comment_id := internal.UUIDGen()
+	err = app.users.InsertComment(comment_id, post_id, user_id, comment)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/home", http.StatusSeeOther)
+}
