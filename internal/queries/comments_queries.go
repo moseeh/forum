afd0bf0f2ea34692037c5a13c193ee98c4b6771e
moonyango <mosesadrian825@gmail.com>
2024-12-26 16:53:24 +0300

    feat: update struct with new values from the comments table like likes dislikes and their respective boolean .. also update get comments function to get all the new values too

--- internal/queries/comments_queries.go
+++ internal/queries/comments_queries.go
@@ -3,10 +3,14 @@ package db
 import "time"
 
 type Comment struct {
-	Comment_ID string
-	Content    string
-	Username   string
-	CreatedAt  time.Time
+	Comment_ID    string
+	Content       string
+	Username      string
+	CreatedAt     time.Time
+	LikesCount    int
+	DislikesCount int
+	IsLiked       bool
+	IsDisliked    bool
 }
 
 func (m *UserModel) InsertComment(comment_id, post_id, user_id, content string) error {
@@ -24,19 +28,31 @@ func (m *UserModel) InsertComment(comment_id, post_id, user_id, content string) 
 	return nil
 }
 
-func (m *UserModel) GetPostComments(postID string) ([]Comment, error) {
+func (m *UserModel) GetPostComments(postID, currentUserID string) ([]Comment, error) {
 	query := `
-    SELECT 
+    SELECT
         c.comment_id,
         c.content,
         u.username,
-        c.created_at
+        c.created_at,
+        c.likes_count,
+        c.dislikes_count,
+        CASE
+            WHEN cl.user_id IS NOT NULL THEN 1
+            ELSE 0
+        END as is_liked,
+        CASE
+            WHEN cd.user_id IS NOT NULL THEN 1
+            ELSE 0
+        END as is_disliked
     FROM COMMENTS c
     JOIN USERS u ON c.user_id = u.user_id
+    LEFT JOIN COMMENT_LIKES cl ON c.comment_id = cl.comment_id AND cl.user_id = ?
+    LEFT JOIN COMMENT_DISLIKES cd ON c.comment_id = cd.comment_id AND cd.user_id = ?
     WHERE c.post_id = ?
     ORDER BY c.created_at DESC`
 
-	rows, err := m.DB.Query(query, postID)
+	rows, err := m.DB.Query(query, currentUserID, currentUserID, postID)
 	if err != nil {
 		return nil, err
 	}
@@ -50,16 +66,18 @@ func (m *UserModel) GetPostComments(postID string) ([]Comment, error) {
 			&comment.Content,
 			&comment.Username,
 			&comment.CreatedAt,
+			&comment.LikesCount,
+			&comment.DislikesCount,
+			&comment.IsLiked,
+			&comment.IsDisliked,
 		)
 		if err != nil {
 			return nil, err
 		}
 		comments = append(comments, comment)
 	}
-
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
-
 	return comments, nil
 }
