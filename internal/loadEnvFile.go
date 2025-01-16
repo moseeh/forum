d3d0cef5567924bd78e7290089aeb50f9036a35a
moonyango <mosesadrian825@gmail.com>
2025-01-16 14:37:58 +0300

    feat: function to read .env files and set the environment variables

---
+++ internal/loadEnvFile.go
@@ -0,0 +1,38 @@
+package internal
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// LoadEnvFile reads a .env file and sets the environment variables
+func LoadEnvFile(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		// Ignore comments and empty lines
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			log.Printf("Invalid line in .env file: %s", line)
+			continue
+		}
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+		err := os.Setenv(key, value)
+		fmt.Println(err)
+	}
+
+	return scanner.Err()
+}
